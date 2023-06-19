7aad4d433071931cf4824c6816cb066c60e9743a
Samir70 <sbetmouni@talktalk.net>
2023-06-19 16:49:17 +0100

    magic eightball

---
+++ fullStack/javaScript/eightball.js
@@ -0,0 +1,8 @@
+let userName = "";
+console.log(userName ? `Hello ${userName}!` : "Hello!")
+let userQuestion = "What is the point of this?"
+console.log("Your question: ", userQuestion)
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = ['It is certain', 'It is decidedly so', 'Reply hazy try again', 'Cannot predict now', 'Do not count on it', 'My sources say no', 'Outlook not so good', 'Signs point to yes'];
+
+console.log(eightBall[randomNumber])
