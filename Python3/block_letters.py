773bc24daa06103ce3602757712ebb79b0875937
Samir70 <sbetmouni@talktalk.net>
2023-06-22 10:01:34 +0100

    block initials

---
+++ Python3/block_letters.py
@@ -0,0 +1,10 @@
+# Samir Betmouni
+# I've been coding for years!
+
+print(" SSS  BBBB ")
+print("S   S B    B")
+print("S     B    B")
+print(" SSS  BBBB")
+print("    S B    B")
+print("S   S B    B")
+print(" SSS  BBBB")
