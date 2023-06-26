632115f8e558c311dd9a7c29018d087c2a532f0c
Samir70 <sbetmouni@talktalk.net>
2023-06-26 17:13:22 +0100

    tarot

---
+++ Python3/tarot.py
@@ -0,0 +1,12 @@
+import random
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+cards = random.sample(range(1, 23), 3)
+print(cards)
+spread = {}
+spread["past"] = tarot.pop(cards[0])
+spread["present"] = tarot.pop(cards[1])
+spread["future"] = tarot.pop(cards[2])
+
+for key, value in spread.items():
+  print(f"Your {key} is the {value} card.")
