80c248aa15cd20961b29841d59efa9b09e107ecb
Samir70 <sbetmouni@talktalk.net>
2023-07-09 14:07:54 +0100

    define options for three parts to sentence

---
+++ fullStack/mixedMessages/sentenceParts.js
@@ -0,0 +1,17 @@
+const starters = [
+  "Once upon a time",
+  "One fine day",
+  "Late at night"
+];
+
+const middles = [
+  " the sun rose unexpectedly and ",
+  ", when everyone was happy, ",
+  " on a busy and noisy road, "
+]
+
+const endings = [
+  "all was darkness.",
+  "the story ended suddenly.",
+  "we all began to laugh."
+]
