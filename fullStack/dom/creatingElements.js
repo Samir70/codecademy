175b9cfeeadd70567bf5626fe7926143e9f936fa
Samir70 <sbetmouni@talktalk.net>
2023-07-10 08:52:46 +0100

    removeChild

--- fullStack/dom/creatingElements.js
+++ fullStack/dom/creatingElements.js
@@ -2,4 +2,5 @@ let newAttraction = document.createElement("li")
 newAttraction.id = "vespa"
 newAttraction.innerHTML = "Rent a Vespa"
 document.getElementById("italy-attractions").append(newAttraction)
+document.getElementById("italy-attractions").removeChild(newAttraction)
 
