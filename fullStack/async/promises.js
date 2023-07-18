63a07febc7cc32ffe3c482a1cbcd2f1191081627
Samir70 <sbetmouni@talktalk.net>
2023-07-18 13:30:13 +0100

    promises and checking stock

---
+++ fullStack/async/promises.js
@@ -0,0 +1,42 @@
+const executorFunction = (resolve, reject) => {
+  if (someCondition) {
+      resolve('I resolved!');
+  } else {
+      reject('I rejected!'); 
+  }
+}
+const myFirstPromise = new Promise(executorFunction);
+
+///////////////////////////
+const returnPromiseFunction = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(( ) => {resolve('I resolved!')}, 1000);
+  });
+};
+ 
+const prom = returnPromiseFunction();
+
+////////////////
+let prom2 = new Promise((resolve, reject) => {
+  let num = Math.random();
+  if (num < .5 ){
+    resolve('Yay!');
+  } else {
+    reject('Ohhh noooo!');
+  }
+});
+ 
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+ 
+const handleFailure = (rejectionReason) => {
+  console.log(rejectionReason);
+};
+ 
+prom2.then(handleSuccess, handleFailure);
+
+// or
+prom2.then(handleSuccess).then(null, handleFailure)
+// or 
+prom2.then(handleSuccess).catch(handleFailure)
