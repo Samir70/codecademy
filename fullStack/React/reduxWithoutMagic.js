42b13a0c62964c6f22add7a75f78f2c66544940f
Samir70 <sbetmouni@talktalk.net>
2023-08-09 13:45:33 +0100

    redux without the magic

---
+++ fullStack/React/reduxWithoutMagic.js
@@ -0,0 +1,51 @@
+const initialState = {
+  todos: [{
+    text: 'Eat food',
+    completed: true
+  }, {
+    text: 'Exercise',
+    completed: false
+  }],
+  visibilityFilter: 'SHOW_COMPLETED'
+}
+
+/**
+ * Actions could be:
+    { type: 'ADD_TODO', text: 'Go to swimming pool' }
+    { type: 'TOGGLE_TODO', index: 1 }
+    { type: 'SET_VISIBILITY_FILTER', filter: 'SHOW_ALL' }
+ */
+
+// seperate reducers
+function visibilityFilter(state = 'SHOW_ALL', action) {
+  if (action.type === 'SET_VISIBILITY_FILTER') {
+    return action.filter
+  } else {
+    return state
+  }
+}
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([{ text: action.text, completed: false }])
+    case 'TOGGLE_TODO':
+      return state.map((todo, index) =>
+        action.index === index
+          ? { text: todo.text, completed: !todo.completed }
+          : todo
+      )
+    default:
+      return state
+  }
+}
+
+
+// combined:
+function todoApp(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+  }
+}
+
