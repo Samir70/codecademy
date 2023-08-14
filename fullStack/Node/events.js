8a5c4e614ac841648224ac8b67f82c3c6ca5762a
Samir70 <sbetmouni@talktalk.net>
2023-08-14 11:04:40 +0100

    events module

---
+++ fullStack/Node/events.js
@@ -0,0 +1,12 @@
+let events = require('events');
+let myEmitter = new events.EventEmitter();
+
+let newUserListener = (data) => {
+  console.log(`We have a new user: ${data}.`);
+};
+ 
+// Assign the newUserListener function as the listener callback for 'new user' events
+myEmitter.on('new user', newUserListener)
+ 
+// Emit a 'new user' event
+myEmitter.emit('new user', 'Lily Pad') //newUserListener will be invoked with 'Lily Pad'
