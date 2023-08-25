ecb0e1a16a367864763b348df591eef11d0225e7
Samir70 <sbetmouni@talktalk.net>
2023-08-25 12:09:37 +0100

    quote keeper server tests

--- fullStack/testingHeadless/quoteKeeper.js
+++ fullStack/testingHeadless/quoteKeeper.js
@@ -1,5 +1,5 @@
 const {assert} = require('chai');
-
+// Feature level tests
 describe("User visits root", () => {
   describe('User can enter quote data', () => {
     it('User can supply quote with author', () => {
@@ -15,5 +15,38 @@ describe("User visits root", () => {
       assert.include(browser.getText("#quotes"), attributed);
       assert.include(browser.getText("#quotes"), source);
     })
+  })
+})
+
+// server level tests
+
+// test/routes/index-test.js
+// const {assert} = require('chai');
+const request = require('supertest');
+const {jsdom} = require('jsdom');
+
+const app = require('../../app');
+
+const parseTextFromHTML = (htmlAsString, selector) => {
+    const selectedElement = jsdom(htmlAsString).querySelector(selector);
+    if (selectedElement !== null) {
+      return selectedElement.textContent;
+    } else {
+      throw new Error(`No element with selector ${selector} found in HTML string`);
+    }
+};
+
+describe("/", () => {
+  describe("POST new request", () => {
+    it("creates a new quote", async () => {
+      const quote = "hello world";
+      const attributed = "almost everyone";
+      const source = "everywhere";
+      const response = await request(app).post('/').type('form').send({quote, attributed, source})
+    assert.equal(response.status, 200);
+    assert.include(parseTextFromHTML(response.text, '#quotes'), quote)
+    assert.include(parseTextFromHTML(response.text, '#quotes'), attributed)
+    assert.include(parseTextFromHTML(response.text, '#quotes'), source)
+    });
   })
 })
